43d3fd76c2ff2afeddade7224c4a50c1bcefbef0
Tobias Bielohlawek <tobi@rngtng.com>
2022-09-27 20:23:10 +0200

    Update

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "yomu"
+gem "standard"
+gem "pry"
+gem "terminal-table"
---
+++ inspect.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+
+require 'terminal-table'
+# require 'poppler'
+require 'date'
+require 'yomu'
+
+def show(file)
+  # document = Poppler::Document.new(file)
+  # a = document.map { |page| page.get_text }.join
+
+  # reader = PDF::Reader.new(file)
+  # reader.pages[0].text
+
+data = File.read file
+text = Yomu.read :text, data
+require "pry";binding.pry
+end
+
+
+rows = []
+Dir["files/*.pdf"] + Dir["files/**/*.pdf"].each do |file|
+  next unless file.include?("rei")
+  filename = file.gsub(/-21-?n?\.pdf/, '-2021').gsub('-22.pdf', '-2022')
+  date = Date.parse(filename).strftime("%d.%m.%Y") rescue nil
+  next unless date
+  # require "pry";binding.pry
+  rows << [
+    date,
+    show(file)
+  ]
+end
+
+puts Terminal::Table.new :rows => rows
+
+# require "pry";binding.pry
