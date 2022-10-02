678b21815f890d8f541172512fd79c69c0f95b57
Tobias Bielohlawek <tobi@rngtng.com>
2022-10-02 09:55:41 +0200

    Next

--- inspect.rb
+++ inspect.rb
@@ -6,8 +6,30 @@ require 'terminal-table'
 require 'date'
 
 def show(file)
-  data = File.read file
-  data.split("preis").size
+  data = File.read(file)
+  data = data.split("www.bdk-schoeneberg.de").last.strip.split("\n")
+  data.select! { |line| line.include?("€") || line.include?("Kol") }
+  data = combine(data).sort
+  data = split(data)
+  data.join("\n")
+end
+
+def split(data)
+  data.each do |l|
+    require "pry";binding.pry
+    l.scan(/[a-züäö .]+/i)
+  end
+end
+
+def combine(data)
+  # require "pry";binding.pry
+  half = data.size / 2
+  combined = data[0...half].map.with_index do |line, index|
+    line + data[half + index]
+  end
+  # require "pry";binding.pry if data.include?("Kol. Frohsinn 46")
+  return data if combined.first.nil? || combined.first.count("€") > 1 || combined.first.split("Kol").size > 2
+  combined
 end
 
 rows = []
@@ -15,6 +37,7 @@ Dir["files/old/*.txt"].each do |file|
   date = Date.parse(file) rescue nil
   rows << [
     date,
+    file,
     show(file)
   ]
 end
