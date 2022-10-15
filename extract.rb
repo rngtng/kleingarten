baa6ee0cd2234fc3f08f529ef3a53443b053dff8
Tobias Bielohlawek <tobi@rngtng.com>
2022-10-15 07:53:12 +0200

    Updates

---
+++ extract.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+
+require "git"
+require "json"
+
+mapping = {}
+Git.open(".").log(1000).each do |commit|
+  commit.diff_parent.map do |diff|
+    if diff.path.include?("pdf")
+      mapping[diff.path] ||= []
+      mapping[diff.path] << commit.date
+    end
+  end
+end
+
+puts mapping.to_json
--- inspect.rb
+++ inspect.rb
@@ -2,12 +2,15 @@
 
 require "bundler/setup"
 
-require 'terminal-table'
-require 'date'
-require 'json'
+require "terminal-table"
+require "date"
+require "json"
+
+# https://bdk-schoeneberg.de/uebersicht/
 
 class Entry
   attr_reader :files, :name
+  attr_accessor :all_dates
 
   def initialize(name, nr, prize, file)
     @name = norm(name)
@@ -17,7 +20,7 @@ class Entry
   end
 
   def dates
-    @files.map { |f| Date.parse(f) }
+    @dates ||= @files.map { |f| Date.parse(f) }.sort
   end
 
   def to_s
@@ -32,7 +35,7 @@ class Entry
       .gsub(/ P(arz)?/, "")
       .gsub("Kleingartenanlage", "")
       .gsub(" - ", "-")
-      .gsub("-", " ")
+      .tr("-", " ")
       .gsub("Ziegenweidde", "Ziegenweide")
       .gsub("Alt Z", "Alte Z")
       .gsub("Schoeneberg", "Schöneberg")
@@ -45,7 +48,6 @@ def lines(file)
   data = File.read(file)
   data = data.split("www.bdk-schoeneberg.de").last.strip.split("\n")
   data.select! { |line| line.include?("€") || line.include?("Kol") }
-  combine(data)
 end
 
 def combine(data)
@@ -60,7 +62,7 @@ end
 @entries = {}
 Dir["files/old/*.txt"].each do |file|
   # `cp #{file.gsub(".txt", ".pdf")} files/norm/#{Date.parse(file)}.pdf`
-  lines(file).map do |line|
+  combine(lines(file)).map do |line|
     name, nr, prize = *line.scan(/([^\d]+)(\d+)[^\d]+([\d.]+)/i).first
     entry = Entry.new(name, nr, prize.delete("."), file)
     if @entries[entry.to_s]
@@ -71,27 +73,30 @@ Dir["files/old/*.txt"].each do |file|
   end
 end
 
+TODAY = Date.today
+
 @kols = @entries.values.map(&:name).uniq.sort
 @dates = @entries.values.map(&:dates).flatten.uniq.sort
 
-a = @entries.sort_by { |k,v| v.dates.sort.first }
-
+@entries.values.each do |e|
+  e.all_dates = e.dates.map do |date|
+    [date, @dates.fetch(@dates.index(date) + 1, TODAY)]
+  end.flatten.uniq.sort
+end
 
 @rows = []
 @rows << ["Kol"] + @dates
-Hash[a].each do |key, entry|
+@entries.values.sort_by { |v| v.dates.min }.each do |entry|
   @rows << [entry.to_s] + @dates.map do |date|
     entry.dates.include?(date) ? 1 : 0
   end
 end
-
 
 # @dates.uniq.sort.each do |date|
 #   k = @kols.each_with_object({}) { |name, hash| hash[name] = 0 }
 #   @entries.each do |key, entry|
 #     k[entry.name] += 1 if entry.dates.include?(date)
 #   end
-
 # end
 
 # @rows.transpose.each do |row|
@@ -99,35 +104,50 @@ end
 # end
 # puts Terminal::Table.new :rows => @rows
 
-series = @entries.values.map do |e|
-  { name: e.to_s, data: @dates.map { |d| [d.to_time.to_i * 1000, e.dates.include?(d) ? 1 : 0]}  }
+series = @entries.values.sort_by { |e| [e.all_dates.size, e.all_dates.first] }.map do |e|
+  {name: e.to_s, data: e.all_dates.map { |d| [d.to_time.to_i * 1000, 1] }}
 end
 
-
 puts <<~HTML
-<html>
-  <script src="https://code.highcharts.com/highcharts.js"></script>
-  <body>
-    <div id="container" style="width:100%; height:400px;"></div>
-    <script>
-      document.addEventListener('DOMContentLoaded', function () {
-        const chart = Highcharts.chart('container', {
-            chart: {
-              type: 'area',
-              zoomType: 'x'
-          },
-            xAxis: {
-              type: 'datetime'
-            },
-            plotOptions: {
-              area: {
-                  stacking: 'normal',
-              }
-            },
-            series: #{series.to_json}
+  <html>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <body>
+      <div id="container" style="width:100%; height:400px;"></div>
+      <script>
+        document.addEventListener('DOMContentLoaded', function () {
+          const chart = Highcharts.chart('container', {
+              title: {
+                text: 'Freie Kleingärten'
+              },
+              legend: {
+                enabled: false
+              },
+              chart: {
+                type: 'area',
+                zoomType: 'x'
+              },
+              xAxis: {
+                type: 'datetime'
+              },
+              plotOptions: {
+                area: {
+                    stacking: 'normal',
+                },
+                series: {
+                  marker: {
+                    enabled: false,
+                    states: {
+                      hover: {
+                          enabled: false
+                      }
+                    }
+                  }
+                }
+              },
+              series: #{series.to_json}
+          });
         });
-      });
-    </script>
-  </body>
-</html>
+      </script>
+    </body>
+  </html>
 HTML
